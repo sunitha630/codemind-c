d735f95391b9d16b0d3552a8a24b31d20ddfd50d
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2023-05-20 22:57:17 +0530

    #include <stdio.h>
    int isUgly(int num) {
        if (num <= 0) {
            return 0;
        }
        while (num % 2 == 0) {
            num /= 2;
        }
        while (num % 3 == 0) {
            num /= 3;
        }
        while (num % 5 == 0) {
            num /= 5;
        }
        return num == 1;
    }
    
    int main() {
        int num;
        scanf("%d", &num);
        
        if (isUgly(num)) {
            printf("Ugly Number");
        } else {
            printf("Not Ugly Number");
        }
        
        return 0;
    }

---
+++ Ugly_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isUgly(int num) {
+    if (num <= 0) {
+        return 0;
+    }
+    while (num % 2 == 0) {
+        num /= 2;
+    }
+    while (num % 3 == 0) {
+        num /= 3;
+    }
+    while (num % 5 == 0) {
+        num /= 5;
+    }
+    return num == 1;
+}
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    
+    if (isUgly(num)) {
+        printf("Ugly Number");
+    } else {
+        printf("Not Ugly Number");
+    }
+    
+    return 0;
+}
