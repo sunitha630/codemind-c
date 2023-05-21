12f4939d593ab988ba1e9a53bf15afb75b6c7e12
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2023-05-21 18:00:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int s=a[0];     for(int i=0;i<n;i++)     {         if(a[i]<s)         {             s=a[i];         }     }     printf("%d",s); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int s=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<s)
+        {
+            s=a[i];
+        }
+    }
+    printf("%d",s);
+}
