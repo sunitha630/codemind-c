842636544cf1b7dcfd7e83b52d465a4428cd9968
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2023-05-21 09:44:15 +0530

    #include<stdio.h> int main() {     int n,sum=10;     scanf("%d",&n);     while(sum>=10)     {         sum=0;         while(n!=0)         {             int r=n%10;             sum=sum+(r*r);             n/=10;         }         n=sum;     }     if(sum==1 || sum==7)     printf("True");     else     printf("False"); }

---
+++ happy_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=10;
+    scanf("%d",&n);
+    while(sum>=10)
+    {
+        sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            sum=sum+(r*r);
+            n/=10;
+        }
+        n=sum;
+    }
+    if(sum==1 || sum==7)
+    printf("True");
+    else
+    printf("False");
+}
