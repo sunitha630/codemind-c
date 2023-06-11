44424dda0585e1845dab0e55ac5c0bfb43f57ae2
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2023-06-11 16:19:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int sum=10;     while (sum>=10)     {         sum=0;         while(n!=0)         {            int r=n%10;             sum=sum+r;             n/=10;         }         n=sum;     }     printf("%d",n);
    }

--- Add_Digits.c
+++ Add_Digits.c
@@ -4,16 +4,19 @@ int main()
     int n;
     scanf("%d",&n);
     int sum=10;
-    while(sum>=10)
+    while (sum>=10)
     {
         sum=0;
         while(n!=0)
         {
-            int r=n%10;
+           int r=n%10;
             sum=sum+r;
             n/=10;
         }
         n=sum;
     }
     printf("%d",n);
+    
+    
+    
 }
